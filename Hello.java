e1c8295fda7169ef3fa5b649dc3f4cd13fb3de09
parakhtripathi0106-rgb <parakhtripathi0106@gmail.com>
2026-09-08 00:37:06 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+    void display(){
+        System.out.println("Helloworld!");
+    }
+}
+
+public class HelloWorldUsingObjectsAndMethodsCalling {
+    public static void main(String[] args) {
+        Hello obj=new Hello();
+        obj.display();
+    }
+}
